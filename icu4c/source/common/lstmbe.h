d7d4d3a5899afe6956d26eb95e3ae75484121430
Frank Tang <ftang@chromium.org>
2021-04-05 17:30:12 -0700

    ICU-21569 LSTM Part 2- include C++ implementation
    ICU-21569 Fix virtual
    
    ICU-21569 Fix dependency issue
    
    ICU-21569 fix memory leak
    
    ICU-21569 fix leak
    
    ICU-21569 Refactor
    
    ICU-21569 Add test resources
    
    ICU-21569 Refactoring
    
    Refactor for test
    
    Add code
    
    Add test

---
+++ icu4c/source/common/lstmbe.h
@@ -0,0 +1,74 @@
+// © 2021 and later: Unicode, Inc. and others.
+// License & terms of use: http://www.unicode.org/copyright.html
+
+#ifndef LSTMBE_H
+#define LSTMBE_H
+
+#include "unicode/uniset.h"
+#include "unicode/ures.h"
+#include "unicode/utext.h"
+#include "unicode/utypes.h"
+
+#include "brkeng.h"
+#include "dictbe.h"
+#include "uvectr32.h"
+
+U_NAMESPACE_BEGIN
+
+class Vectorizer;
+class LSTMData;
+
+/*******************************************************************
+ * LSTMBreakEngine
+ */
+
+/**
+ * <p>LSTMBreakEngine is a kind of DictionaryBreakEngine that uses a
+ * LSTM to determine language-specific breaks.</p>
+ *
+ * <p>After it is constructed a LSTMBreakEngine may be shared between
+ * threads without synchronization.</p>
+ */
+class LSTMBreakEngine : public DictionaryBreakEngine {
+public:
+    /**
+     * <p>Constructor.</p>
+     */
+    LSTMBreakEngine(const LSTMData* data, const UnicodeSet& set, UErrorCode &status);
+
+    /**
+     * <p>Virtual destructor.</p>
+     */
+    virtual ~LSTMBreakEngine();
+
+    virtual const UChar* name() const;
+
+protected:
+    /**
+     * <p>Divide up a range of known dictionary characters handled by this break engine.</p>
+     *
+     * @param text A UText representing the text
+     * @param rangeStart The start of the range of dictionary characters
+     * @param rangeEnd The end of the range of dictionary characters
+     * @param foundBreaks Output of C array of int32_t break positions, or 0
+     * @return The number of breaks found
+     */
+     virtual int32_t divideUpDictionaryRange(UText *text,
+                                             int32_t rangeStart,
+                                             int32_t rangeEnd,
+                                             UVector32 &foundBreaks ) const;
+private:
+    const LSTMData* fData;
+    const Vectorizer* fVectorizer;
+};
+
+
+const LanguageBreakEngine* CreateLSTMBreakEngine(UScriptCode script, const LSTMData* data, UErrorCode& status);
+const LSTMData* CreateLSTMData(UResourceBundle* rb, UErrorCode& status);
+const LSTMData* CreateLSTMDataForScript(UScriptCode script, UErrorCode& status);
+void DeleteLSTMData(const LSTMData* data);
+
+
+U_NAMESPACE_END
+
+#endif  /* LSTMBE_H */
---
+++ icu4c/source/test/intltest/lstmbetst.h
@@ -0,0 +1,51 @@
+// © 2021 and later: Unicode, Inc. and others.
+// License & terms of use: http://www.unicode.org/copyright.html
+
+#ifndef LSTMBETEST_H
+#define LSTMBETEST_H
+
+#include "unicode/utypes.h"
+
+#if !UCONFIG_NO_BREAK_ITERATION
+
+#include <memory>
+
+#include "intltest.h"
+
+#include "unicode/uscript.h"
+
+struct TestParams;
+
+U_NAMESPACE_BEGIN
+class LanguageBreakEngine;
+U_NAMESPACE_END
+
+
+/**
+ * Test the LSTMBreakEngine class giving different rules
+ */
+class LSTMBETest: public IntlTest {
+public:
+
+    LSTMBETest();
+    virtual ~LSTMBETest();
+
+    void runIndexedTest( int32_t index, UBool exec, const char* &name, char* par = NULL );
+
+    void TestThai();
+    void TestBurmese();
+    void runTestFromFile(const char* filename);
+
+    // TO BE DELETED after landing https://github.com/unicode-org/icu/pull/1700/files
+    UChar *ReadAndConvertFile(const char *fileName, int &ulen, const char *encoding, UErrorCode &status);
+/***********************/
+private:
+    const LanguageBreakEngine* createEngineFromTestData(const char* model, UScriptCode script, UErrorCode& status);
+
+    // Test parameters, from the test framework and test invocation.
+    const char* fTestParams;
+};
+
+#endif /* #if !UCONFIG_NO_BREAK_ITERATION */
+
+#endif
