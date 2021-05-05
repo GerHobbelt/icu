5ebcf32709b712f792074a6f0dc9a18cb26afd29
Ger Hobbelt <ger@hobbelt.com>
2021-05-06 01:08:29 +0200

    bit of cleanup

--- icu4c/source/common/unicode/uconfig.h
+++ icu4c/source/common/unicode/uconfig.h
@@ -1,9 +1,3 @@
-/* ICU customizations: put these lines at the top of uconfig.h */
-/* -DU_HAVE_LIB_SUFFIX=1 */
-//#define U_HAVE_LIB_SUFFIX 1
-/* -DU_LIB_SUFFIX_C_NAME=_swift */
-//#define U_LIB_SUFFIX_C_NAME _swift 
-
 // © 2016 and later: Unicode, Inc. and others.
 // License & terms of use: http://www.unicode.org/copyright.html
 /*  
