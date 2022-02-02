5a6fe09d1022bd561d8b61fb92b4c589b453c04b
Marco Dias <mdias@esri.com>
2022-02-02 08:30:04 +0000

    Updates the data file via the following method:
    Switch to unedited ICU branch you have merged into your branch.
    Check the icu4c/source/data/out folder is clean.
    Go to source/allinone and open the solution, build all in x64 release.
    Checkout the icu4c/source/data/filters.json file you wish to use for the data, into this clean branch.
    Edit the filters if you wish to update the data, info is here:  https://unicode-org.github.io/icu/userguide/icu_data/buildtool.html
    
    Open VS-terminal and at icu4c/source/data run 'nmake /f makedata.mak ICUMAKE="c:\runtime\3rdparty\icu\icu4c\source\data" CFG="x64\Release" GENRBOPTS="--omitCollationRules" ICU_DATA_FILTER_FILE=filters.json'
    
    This will build a new data file named icudt(version_num)l.dat
    Goto icu4c/bin64 with vsterm and run 'genccode.exe C:\runtime\3rdparty\icu\icu4c\source\data\out\icudt(versionnum)l.dat'
    This will create a icudt(version_num)l.c file
    
    Rename the file to icudt(version_num).c, by removing the L.
    Place the following lines inside the file, before the line '#include "unicode/umachine.h"'
    
     '#include <stdint.h>
     typedef uint16_t char16_t;'
    
     Rename the data variable to icudt(version_num)_dat by removing the l.
     Save this C file somewhere else, and copy it into your new dirty branch
     replacing the previous data file at icu4c/source/data
     Update the top level icu.lua file to point to the new data file.
    
     Do a complete clean of all intermediate files and then do a complete
     build. Hopefully everything works!

--- conanfile_rtc.py
+++ conanfile_rtc.py
@@ -3,7 +3,7 @@ from conans import ConanFile
 
 class ICUConan(ConanFile):
     name = "icu"
-    version = "69.2"
+    version = "70.1"
     url = "https://github.com/Esri/icu/tree/runtimecore"
     license = "https://github.com/Esri/icu/blob/runtimecore/icu4c/LICENSE"
     description = "International Components for Unicode."
